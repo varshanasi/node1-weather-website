826997955ae286e4d64ae27d7b1a5cdac709a680
varsha.nasi98@gmail.com <varsha.nasi98@gmail.com>
2021-06-26 16:24:19 +0530

    Setup map for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -11,7 +11,7 @@ messageTwo.textContent=''
 weatherForm.addEventListener('submit',(e)=>{
     e.preventDefault()
     const location= search.value
-    fetch('http://localhost:3000/weather?address='+location).then((response)=>
+    fetch('/weather?address='+location).then((response)=>
     { console.log('')
         response.json().then((data)=>{
             if(data.error){
